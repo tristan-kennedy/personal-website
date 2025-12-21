e186532486f990446b56b8397d2aaa44ab28178e
Tristan Kennedy <tdouglaskennedy@gmail.com>
2025-12-21 10:37:45 -0600

    WIP but I'm going home

---
+++ src/content/config.ts
@@ -0,0 +1,36 @@
+import { defineCollection, z } from "astro:content";
+
+const projects = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      description: z.string(),
+      tech: z.array(z.string()).optional(),
+      image: image(),
+    }),
+});
+
+const posts = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      description: z.string(),
+      length: z.string(),
+      image: image(),
+    }),
+});
+
+const experiments = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      image: image(),
+    }),
+});
+
+export const collections = { projects, posts, experiments };
